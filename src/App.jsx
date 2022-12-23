63dc28e64be9dc6d408af7b3dd5d8be5ce4267e3
T4P4N <tapank415@gmail.com>
2022-12-23 15:11:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import { useRef, useState } from "react";
+import Patrick from "./Templates/Patrick";
+import { FiInfo } from "react-icons/fi";
+import exportAsImage from "./exportAsImage";
+
+function App() {
+  const [title, setTitle] = useState("Patrick Bateman");
+  const [description, setDescription] = useState(`Vice President`);
+  const [addresss, setAddresss] = useState(
+    "358 Exchange Place New York, N.Y. 100099 FAX 212 555 6390"
+  );
+  const [phone, setPhone] = useState("212556342");
+  const [brandName, setBrandName] = useState("Pierce & Pierce");
+  const [brandline, setBrandline] = useState("Mergers and Acquisitions");
+
+  const [gradient, setGradient] = useState(
+    "bg-gradient-to-r from-gray-800 via-gray-900 to-black"
+  );
+  const [fontColor, setFontColor] = useState("text-white");
+  const exportRef = useRef();
+  return (
+    <>
+      <Patrick
+        title={title}
+        description={description}
+        addresss={addresss}
+        phone={phone}
+        brandName={brandName}
+        brandline={brandline}
+        gradient={gradient}
+        fontColor={fontColor}
+        childRef={exportRef}
+      />
+      <div className="flex flex-col items-start justify-center form-wrapper gap-2 w-100">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Designation"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Address"
+          onChange={(e) => {
+            setAddresss(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          onChange={(e) => {
+            setPhone();
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Company"
+          onChange={(e) => {
+            setBrandName(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Tag Line"
+          onChange={(e) => {
+            setBrandline(e.target.value);
+          }}
+        />
+        <p className="flex gap-1 items-center text-slate-500 outline outline-2 p-2 rounded">
+          <FiInfo />
+          Copy paste from tailwind classes from{" "}
+          <a href="https://hypercolor.dev/" className="text-blue underline">
+            Hyper Color{" "}
+          </a>
+          to below
+        </p>
+        <input
+          type="text"
+          placeholder="Gradient"
+          onChange={(e) => {
+            setGradient(e.target.value);
+          }}
+        />
+        <p className="flex gap-1 items-center text-slate-500 outline outline-2 p-2 rounded">
+          <FiInfo />
+          Copy paste from tailwind classes from{" "}
+          <a
+            href="https://tailwindcss.com/docs/text-color"
+            className="text-blue underline"
+          >
+            Here{" "}
+          </a>
+          to below
+        </p>
+        <input
+          type="text"
+          placeholder="Font Color"
+          onChange={(e) => {
+            setFontColor(e.target.value);
+          }}
+        />
+        <button
+          className="bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white
+          p-5 rounded font-paul
+          "
+          onClick={() => exportAsImage(exportRef.current, "Paul Allen")}
+        >
+          Export as Image
+        </button>
+        <br />
+        <p className="text-center text-sm font-paul">
+          Made by <a href="https://t4p4n.github.io">Paul Allen</a>
+        </p>
+        <br />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Templates/Patrick.jsx
@@ -0,0 +1,50 @@
+function Patrick({
+  title,
+  description,
+  addresss,
+  phone,
+  brandName,
+  brandline,
+  gradient,
+  fontColor,
+  childRef,
+}) {
+  return (
+    <div
+      className="card-area"
+      ref={childRef}
+      style={{
+        maxWidth: "500px",
+        marginInline: "auto",
+        marginTop: "1em",
+      }}
+    >
+      <div
+        className={`relative ${fontColor} ${gradient}`}
+        style={{
+          maxWidth: "500px",
+          height: "276px",
+          fontFamily: "'Cinzel', serif",
+        }}
+      >
+        <div className="flex items-start justify-between p-2">
+          <p className="text">{phone}</p>
+
+          <div className="text-center">
+            <div className="text-lg">{brandName}</div>
+            <div className="text-xs">{brandline}</div>
+          </div>
+        </div>
+        <div className="absolute m-auto left-0 right-0 bottom-28">
+          <div className="text-center text-xl">{title}</div>
+          <div className="text-center">{description}</div>
+        </div>
+        <div className="text-sm text-center absolute bottom-5 m-auto left-0 right-0">
+          {addresss}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Patrick;
